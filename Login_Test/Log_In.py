3f1677209d111213ce123821eeea52df883ff2d0
luke906 <chargerunit@gmail.com>
2017-09-19 19:11:41 +0900

    Update Log_In.py

--- Login_Test/Log_In.py
+++ Login_Test/Log_In.py
@@ -1,36 +1,46 @@
-
-import WebDriver_Class
+import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+str_Chrome_Path = "C:/Users\Lule LEE/PycharmProjects/Selenium_Driver/chromedriver_win32/chromedriver"
 
-str_Chrome_Path = "C:/Users/charg/Selenium_Driver/chromedriver_win32/chromedriver"
-
+# str_NaverLogin_URL = "https://nid.naver.com/nidlogin.login"
 str_AirBitClub_Login_URL = "https://www.bitbackoffice.com/auth/login"
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-infobars")
+browser = webdriver.Chrome(executable_path = str_Chrome_Path, chrome_options = chrome_options)
+browser.implicitly_wait(3)
+
+# driver = webdriver.Chrome(str_Chrome_Path)
+# driver = webdriver.PhantomJS(str_Phantom_Path)
+# driver.set_window_size(1120, 550)
+
+browser.get(str_AirBitClub_Login_URL)
+print("Driver Inititalized")
+
+# browser.implicitly_wait(3)
+# driver.get(str_NaverLogin_URL)
+# driver.find_element_by_name('id').send_keys('luke906')
+# driver.find_element_by_name('pw').send_keys('newlife8661!')
+
+# driver.implicitly_wait(3)
+
+browser.find_element_by_name("user[username]").send_keys('lsw120300')
+browser.find_element_by_name("user[password]").send_keys('lsw8954!')
+browser.find_element_by_xpath('//*[@id="new_user"]/button').click()
+
+# Move to Wallets
 Wallet_Path = "https://www.bitbackoffice.com/wallets"
-
-"""
-Browser_Class = []
-for index in range(1,5):
-    class_name = "Browser" + str(index)
-    print(class_name)
-    class_name = WebDriver_Class.WebDriver(str_Chrome_Path)
-    Browser_Class[].append(class_name)
-    class_name.move_to_url(str_AirBitClub_Login_URL)
-"""
+browser.get(Wallet_Path)
 
 
+# print("Airbit Club Login Succeed!!")
+# print(driver.page_source)
 
+# client = requests.session()
+# client.get("https://www.bitbackoffice.com/wallets")
+# html_source = client.text
+# print(html_source)
 
-Browser1 = WebDriver_Class.WebDriver(str_Chrome_Path)
-Browser1.move_to_url(str_AirBitClub_Login_URL)
-
-Browser1.send_key_by_name("user[username]", "lsw120300")
-Browser1.send_key_by_name("user[password]", "lsw8954!")
-Browser1.send_click_event('//*[@id="new_user"]/button')
-Browser1.move_to_url(Wallet_Path)
-
-
-"""
-wallet_html_source = Browser1.get_html_source()
-soup = BeautifulSoup(wallet_html_source, 'html.parser')
-print(soup.find_all('p', {'class':'result btc-container'}))
-"""
+# driver.quit()
