a776c0ca63e5b611d08fcbc53d13fac10d909e2b
Luke LEE <chargerunit@gmail.com>
2017-09-20 18:38:28 +0900

    .

--- Binary Simulation/Binary_Simulation.py
+++ Binary Simulation/Binary_Simulation.py
@@ -20,16 +20,16 @@ Account_Level_Node_List = []
 Cur_Level_Value = 0
 
 
+# 계좌 생성 함수
+# 호출시 자동으로 레벨을 계산하며 순차적으로 배치가 된다.
 def create_account():
     global Level_Complete_Flag
     global Total_Node_Count
     global Level_Node_Count
-    global Account_Node_List
     global Cur_Level_Value
     global Account_Node_Dic
     global Account_Level_Node_Key_Dic
     global Account_Level_Node_List
-
 
     new_node_account = None
 
@@ -49,7 +49,6 @@ def create_account():
     Account_Level_Node_List.append(Total_Node_Count)
     Account_Level_Node_Key_Dic[Cur_Level_Value] = Account_Level_Node_List
 
-
     # 현재의 레벨에 노드가 FULL 인지 검사
     if Level_Node_Count == (2 ** Cur_Level_Value):
         Level_Node_Count = 0
@@ -58,18 +57,62 @@ def create_account():
         Cur_Level_Value += 1
 
 
+
+
+def connect_node_account():
+
+    if Cur_Level_Value == 0:
+        pass
+
+    # 마지막으로 생성된 키는 현재 작업할 노드의 키이다.
+    node_key = Total_Node_Count
+
+    # 자식으로 추가될 계좌 노드를 구한다.
+    child_node = Account_Node_Dic[node_key]
+
+    # 전달된 현재 레벨의 상위 단계 레벨안에 들어 있는 노드의 총 갯수를 구한다.
+    index = Cur_Level_Value-1
+    node_count_in_level = len(Account_Level_Node_Key_Dic[index])
+
+    for i in range(0, node_count_in_level+1):
+
+        # 구할 보모 노드의 키를 구한다.
+        node_key = Account_Level_Node_Key_Dic[index][i]
+
+        # 부모 노드를 구한다.
+        parent_node = Account_Node_Dic[node_key]
+
+        # 해당 부모의 왼쪽에 노드가 있는지 검사하고 없다면 부모 노드의 left로 셋팅한다.
+        if parent_node.left_child != None:
+            parent_node.Set_Left_Child_Node(child_node)
+            Account_Node_Dic[node_key] = parent_node
+            break
+
+        # 해당 부모의 오른쪽에 노드가 있는지 검사하고 없다면 부모 노드의 right로 셋팅한다.
+        elif parent_node.right_child != None:
+            parent_node.Set_right_Child_Node(child_node)
+            Account_Node_Dic[node_key] = parent_node
+            break
+
+
+
+
 def main():
-    for i in range(1, 101):
+    for i in range(1, 8):
         #print(i)
         create_account()
+        connect_node_account()
+
 
     index = None
-    for a in range(0, Cur_Level_Value+1):
+    for a in range(0, Cur_Level_Value):
         index = a
 
         print("%d 대 계좌 목록" % a)
         print(Account_Level_Node_Key_Dic[index])
         print("\n")
+
+
 
 
 if __name__ == '__main__':
--- Binary Simulation/Tree_Class.py
+++ Binary Simulation/Tree_Class.py
@@ -1,10 +1,30 @@
 class binarytree(object):
     def __init__(self, data, level_number, left_child = None, right_child = None):
         self.node_number      = data
-        self.Level            = level_number
+        self.level            = level_number
         self.root_node_number = None
         self.left_child       = left_child
         self.right_child      = right_child
+
+    # R:추천수당  S:후원수당  M:매트릭스 보너스
+
+    def Set_R_Money(self, money):
+        self.r_money = money
+
+    def Set_S_Money(self, money):
+        self.s_money = money
+
+    def Set_M_Money(self, money):
+        self.m_money = money
+
+    def Get_R_Money(self, money):
+        return self.r_money
+
+    def Get_S_Money(self, money):
+        return self.s_money
+
+    def Get_M_Money(self, money):
+        return self.m_money
 
     def Set_Node_Number(self, data):
         self.node_number = data
