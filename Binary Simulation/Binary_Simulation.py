aa1855b47add2d50325fe44620d6d00f70d90ef6
Luke LEE <chargerunit@gmail.com>
2017-09-20 15:06:00 +0900

    .

--- Binary Simulation/Binary_Simulation.py
+++ Binary Simulation/Binary_Simulation.py
@@ -9,8 +9,12 @@ Total_Node_Count = 0
 # 각 레벨에서 생성될 노드 카운트
 Level_Node_Count = 0
 
-# 각각 생성된 계좌를 저장할 리스트
-Account_Node_List = []
+# 각각 생성된 계좌 노드를 저장할 딕셔너리
+Account_Node_Dic = {}
+
+# 각 레벨별 생성된 계좌 노드 키를 저장할 딕셔너리, 임시 리스트
+Account_Level_Node_Key_Dic = {}
+Account_Level_Node_List = []
 
 # 현재 생성할 레벨
 Cur_Level_Value = 0
@@ -22,19 +26,12 @@ def create_account():
     global Level_Node_Count
     global Account_Node_List
     global Cur_Level_Value
+    global Account_Node_Dic
+    global Account_Level_Node_Key_Dic
+    global Account_Level_Node_List
+
 
     new_node_account = None
-
-    # 만일 현재 레벨이 0 이라면 루트 계좌 생성
-    if Cur_Level_Value == 0:
-        new_node_account = Tree_Class.BinaryTree(0, Cur_Level_Value)
-
-    # 만일 현재 레엘이 0보다 크다면 자식 노드 생성
-    elif Cur_Level_Value > 0:
-        new_node_account = Tree_Class.BinaryTree(Total_Node_Count, Cur_Level_Value)
-
-    # 생성된 계좌 리스트에 저장
-    Account_Node_List.append(new_node_account)
 
     # 총 노드 갯수 증가
     Total_Node_Count += 1
@@ -42,18 +39,37 @@ def create_account():
     # 현재 레벨에서 생성된 노드의 갯수 증가
     Level_Node_Count += 1
 
+    # 신규 계좌 생성
+    new_node_account = Tree_Class.binarytree(Total_Node_Count, Cur_Level_Value)
+
+    # 생성된 계좌 저장
+    Account_Node_Dic[Total_Node_Count] = new_node_account
+
+    # 현재 레벨에서 생성된 계좌를 레벨별로 저장
+    Account_Level_Node_List.append(Total_Node_Count)
+    Account_Level_Node_Key_Dic[Cur_Level_Value] = Account_Level_Node_List
+
+
     # 현재의 레벨에 노드가 FULL 인지 검사
     if Level_Node_Count == (2 ** Cur_Level_Value):
         Level_Node_Count = 0
         Level_Complete_Flag[Cur_Level_Value] = True
+        Account_Level_Node_List = []
         Cur_Level_Value += 1
 
 
 def main():
-    for i in range(1, 100):
+    for i in range(1, 101):
+        #print(i)
         create_account()
 
-        # 사용자가 원하는 생성할 계좌의 갯수가 모두 생성 되었다면 종료
+    index = None
+    for a in range(0, Cur_Level_Value+1):
+        index = a
+
+        print("%d 대 계좌 목록" % a)
+        print(Account_Level_Node_Key_Dic[index])
+        print("\n")
 
 
 if __name__ == '__main__':
--- Binary Simulation/Tree_Class.py
+++ Binary Simulation/Tree_Class.py
@@ -1,31 +1,41 @@
-class BinaryTree():
+class binarytree(object):
     def __init__(self, data, level_number, left_child = None, right_child = None):
-        self.node_number = data
-        self.Level = level_number
+        self.node_number      = data
+        self.Level            = level_number
         self.root_node_number = None
-        self.left_child = left_child
-        self.right_child = right_child
-
-    def Get_Node_Number(self):
-        return self.node_number
-
-    def Get_Left_Child_Node(self):
-        return self.left_child
-
-    def Get_Right_Child_Node(self):
-        return self.right_child
+        self.left_child       = left_child
+        self.right_child      = right_child
 
     def Set_Node_Number(self, data):
         self.node_number = data
 
-    def Append_Left_Child_Node(self, sub):
+    def Get_Node_Number(self):
+        return self.node_number
+
+
+    def Set_Left_Child_Node(self, sub):
         self.left_child = sub
 
-    def Append_right_Child_Node(self, sub):
+    def Get_Left_Child_Node(self):
+        return self.left_child
+
+
+    def Set_right_Child_Node(self, sub):
         self.right_child = sub
 
-    def preorder_traverse(tree):
-        if tree == None: return
-        print(tree.node_number)
-        preorder_traverse(tree.left_child)
-        preorder_traverse(tree.right_child)
+    def Get_Right_Child_Node(self):
+        return self.right_child
+
+
+def preorder_traverse(tree, action):
+    if tree == None:
+        return
+
+    action(tree.node_number)
+    preorder_traverse(tree.Get_Left_Child_Node(), action)
+    preorder_traverse(tree.Get_Right_Child_Node(), action)
+
+def ShowIntData(data):
+    print(data)
+
+
