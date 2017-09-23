c15028461628175beebb8f8a547ef822e09b4e76
luke <chargerunit@gmail.com>
2017-09-23 23:29:19 +0900

    .

---
+++ Binary Simulation/BinaryCalc.py
@@ -0,0 +1,228 @@
+from TreeClass import BinaryTree
+import CalcMoney
+import CheckNodeInfo
+
+# 마지막으로 생성된 노드 키
+_Last_Node_Key = -1
+
+# 각 레벨에서 생성될 노드 카운트
+_Level_Node_Count = 0
+
+# 각각 생성된 계좌 노드를 저장할 딕셔너리
+_Account_Node_Dic = {}
+
+# 각 레벨별 생성된 계좌 노드 키를 저장할 딕셔너리, 임시 리스트
+_Account_Level_Node_Key_Dic = {}
+_Account_Level_Node_List = []
+
+# 현재 생성할 레벨
+_Cur_Level_Value = 0
+
+# 각 생성된 레벨의 완성여부 딕셔너리
+_Level_Complete_Flag = {}
+
+_Left_Side_Node_Count_List = []
+_Right_Side_Node_Count_List = []
+
+SAVING_RATIO = 0.2
+RECOMMAND_MONEY = 200
+MATRIX_BONUS = 10
+SUPPORT_ODD_MONEY = 80
+SUPPORT_EVEN_MONEY = 80
+
+
+
+
+# 계좌 생성 함수
+# 호출시 자동으로 레벨을 계산하며 순차적으로 배치가 된다.
+def create_account():
+    global _Level_Complete_Flag
+    global _Last_Node_Key
+    global _Cur_Level_Value
+    global _Account_Node_Dic
+    global _Account_Level_Node_Key_Dic
+    global _Account_Level_Node_List
+    global _Level_Node_Count
+
+    # 생성될 노드키
+    _Last_Node_Key += 1
+
+    # 현재 레벨에서 생성된 노드의 갯수 증가
+    _Level_Node_Count += 1
+
+    # 신규 계좌 생성
+    new_node_account = BinaryTree(_Last_Node_Key, _Cur_Level_Value)
+
+    # 생성된 계좌 저장
+    _Account_Node_Dic[_Last_Node_Key] = new_node_account
+
+    # 현재 레벨에서 생성된 계좌를 레벨별로 저장
+    _Account_Level_Node_List.append(_Last_Node_Key)
+    _Account_Level_Node_Key_Dic[_Cur_Level_Value] = _Account_Level_Node_List
+
+    # 현재의 레벨에 노드가 FULL 인지 검사
+    if _Level_Node_Count == (2 ** _Cur_Level_Value):
+        _Level_Complete_Flag[_Cur_Level_Value] = True
+        _Level_Node_Count = 0
+        _Cur_Level_Value += 1
+
+        # 새로운 레벨 생성시 참조 에러가 나기 때문에 빈 데이터를 셋팅한다.
+        _Account_Level_Node_List = []
+        _Account_Level_Node_Key_Dic[_Cur_Level_Value] = _Account_Level_Node_List
+
+    connect_node_account()
+
+
+def connect_node_account():
+    # 자식으로 추가될 계좌 노드를 구한다.
+    child_node_key = _Last_Node_Key
+    child_node = _Account_Node_Dic[child_node_key]
+
+    if child_node.node_number == 0:
+        return
+
+    # 마지막으로 생성된 노드 레벨의 상위 단계 레벨안에 들어 있는 노드의 총 갯수를 구한다.
+    index = child_node.level - 1
+    node_count_in_level = len(_Account_Level_Node_Key_Dic[index])
+
+    for i in range(0, node_count_in_level):
+
+        # 구할 보모 노드의 키를 구한다.
+        parent_node_key = _Account_Level_Node_Key_Dic[index][i]
+
+        # 부모 노드를 구한다.
+        parent_node = _Account_Node_Dic[parent_node_key]
+
+        # 해당 부모의 왼쪽에 노드가 있는지 검사하고 없다면 부모 노드의 left로 셋팅한다.
+        if parent_node.left_child_have == False:
+            parent_node.set_left_child_node(child_node)
+            _Account_Node_Dic[parent_node_key] = parent_node
+
+            child_node.set_parent_node(parent_node)
+            _Account_Node_Dic[child_node_key] = child_node
+
+            # 추천수당 계산
+            CalcMoney.calc_recommand_money(parent_node, RECOMMAND_MONEY)
+
+            # 매트리스 수당 계산
+            CalcMoney.calc_matrix_money(child_node, MATRIX_BONUS)
+            return
+
+        # 해당 부모의 오른쪽에 노드가 있는지 검사하고 없다면 부모 노드의 right로 셋팅한다.
+        if parent_node.right_child_have == False:
+            parent_node.set_right_child_node(child_node)
+            _Account_Node_Dic[parent_node_key] = parent_node
+
+            child_node.set_parent_node(parent_node)
+            _Account_Node_Dic[child_node_key] = child_node
+
+            # 추천수당 계산
+            CalcMoney.calc_recommand_money(parent_node, RECOMMAND_MONEY)
+
+            # 매트리스 수당 계산
+            CalcMoney.calc_matrix_money(child_node, MATRIX_BONUS)
+            return
+
+
+# 후원수당 계산
+def calc_support_money(base_node_index):
+    # 현재 생성된 마지막 노드의 레벨
+    child_node_object = _Account_Node_Dic[_Last_Node_Key]
+    current_level = child_node_object.level
+
+    if current_level == 0:
+        return
+
+    if _Last_Node_Key < 2:
+        return
+
+    # 현재노드 레벨의 마로위 레벨의 마지막 생성 노드 번호
+    count = len(_Account_Level_Node_Key_Dic[current_level - 1])
+    before_level__Last_Node_Key = _Account_Level_Node_Key_Dic[current_level - 1][count - 1]
+    # print("현재 생성된 레벨 바로 상위 레벨의 마지막 노드 번호: %d", before_level__Last_Node_Key)
+
+    # 모든 노드를 순회 하면서 대실적, 소실적을 구분한다.
+    # 소실적 노드의 갯수가 홀수이면 80
+    # 소실적 노드의 갯수가 짝수이면 90
+
+    index = base_node_index
+
+    # 검색 기준이 되는 키
+    node_object = _Account_Node_Dic[index]
+
+    # 검색 기준이 되는 키의 좌측 모든 노드 갯수를 구한다.
+    left_node_object = node_object.get_left_child_node()
+    preorder_traverse(base_node_index, left_node_object, "left")
+
+    # 검색 기준이 되는 키의 우측 모든 노드 갯수를 구한다.
+    right_node_object = node_object.get_right_child_node()
+    preorder_traverse(base_node_index, right_node_object, "right")
+
+    w_count = {}
+    for lst in _Left_Side_Node_Count_List:
+        try:
+            w_count[lst] += 1
+        except:
+            w_count[lst] = 1
+    print("%d번 노드 왼쪽 노드의 총 갯수:%d " % (base_node_index, len(w_count)))
+
+    w_count = {}
+    for lst in _Right_Side_Node_Count_List:
+        try:
+            w_count[lst] += 1
+        except:
+            w_count[lst] = 1
+    print("%d번 노드 오른쪽 노드의 총 갯수:%d \n" % (base_node_index, len(w_count)))
+
+    preorder_traverse()
+
+
+def preorder_traverse(base_node_index=-1, tree_object=None, flag=None):
+    global _Left_Side_Node_Count_List
+    global _Right_Side_Node_Count_List
+
+    if base_node_index == -1:
+        _Left_Side_Node_Count_List = []
+        _Right_Side_Node_Count_List = []
+
+    if tree_object is None:
+        return
+
+    # print(tree_object.node_number)
+
+    if flag is "left":
+        print("%d번 노드의 LEFT 하위 노드: %d" % (base_node_index, tree_object.node_number))
+        _Left_Side_Node_Count_List.append(tree_object.node_number)
+
+    elif flag is "right":
+        print("%d번 노드의 RIGHT 하위 노드: %d" % (base_node_index, tree_object.node_number))
+        _Right_Side_Node_Count_List.append(tree_object.node_number)
+
+    preorder_traverse(base_node_index, tree_object.get_left_child_node(), flag)
+    preorder_traverse(base_node_index, tree_object.get_right_child_node(), flag)
+
+
+def main():
+    """
+    for i in range(0, 15):
+        # print(i)
+        create_account()
+        connect_node_account()
+    """
+    create_account()
+    create_account()
+    create_account()
+    calc_support_money(0)
+
+    create_account()
+
+    create_account()
+    calc_support_money(1)
+
+    CheckNodeInfo.show_all_node(_Cur_Level_Value, _Account_Level_Node_Key_Dic)
+    CheckNodeInfo.show_node_information(0, _Account_Node_Dic)
+    CalcMoney.show_all_money(_Account_Node_Dic, _Last_Node_Key)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Binary Simulation/TreeClass.py
@@ -0,0 +1,72 @@
+class BinaryTree(object):
+    def __init__(self, node_number, node_level_number, parent_node_number=None, parent=None, left_child=None, right_child=None):
+        self.node_number = node_number
+        self.level = node_level_number
+        self.parent_node = parent
+        self.parent_node_number = parent_node_number
+
+        self.left_child = left_child
+        self.left_child_have = False
+
+        self.right_child = right_child
+        self.right_child_have = False
+
+        self.r_money = 0
+        self.s_money = 0
+        self.m_money = 0
+
+    # R:추천수당  S:후원수당  M:매트릭스 보너스
+
+    def set_parent_node(self, node):
+        self.parent_node = node
+
+    def get_parent_node(self):
+        return self.parent_node
+
+    def set_r_money(self, money):
+        self.r_money = money
+
+    def set_s_money(self, money):
+        self.s_money = money
+
+    def set_m_money(self, money):
+        self.m_money = money
+
+    def get_r_money(self):
+        return self.r_money
+
+    def get_s_money(self):
+        return self.s_money
+
+    def get_m_money(self):
+        return self.m_money
+
+    def set_node_number(self, data):
+        self.node_number = data
+
+    def get_node_number(self):
+        return self.node_number
+
+    def set_left_child_node(self, sub):
+        self.left_child = sub
+        self.left_child_have = True
+
+    def get_left_child_node(self):
+        return self.left_child
+
+    def set_right_child_node(self, sub):
+        self.right_child = sub
+        self.right_child_have = True
+
+    def get_right_child_node(self):
+        return self.right_child
+
+    def preorder_traverse(self, tree, action):
+        if tree == None:
+            return
+        action(tree.node_number)
+        preorder_traverse(tree.Get_Left_Child_Node(), action)
+        preorder_traverse(tree.Get_Right_Child_Node(), action)
+
+    def showintdata(self, data):
+        print(data)
