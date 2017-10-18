0a00f48f70279b1e23bb86c53d86d5a215fea4f0
luke <chargerunit@gmail.com>
2017-10-18 17:58:32 +0900

    .

--- Binary Simulation/CalcMoney.py
+++ Binary Simulation/CalcMoney.py
@@ -118,69 +118,9 @@ def calc_support_money(child_node, _last_node_key,
         print("%d번 부모 계좌에 셋팅될  후원 수당은 : %d" % (parent_object.node_number, temp_s_money))
         parent_object.set_s_money(temp_s_money)
 
-"""
 
-# 계좌를 한꺼번에 셋팅하고 후원수당을 계산하는 함수.
-# 한번에 한계씩 계좌를 셋팅하는것이 아니라 한번에 원하는 수량만큼 계좌를 셋팅하는 경우의 함수
-def calc_support_money_setting(_last_node_key,
-                               _account_level_node_key_dic,
-                               _account_node_dic):
 
-    # 0레벨 부터 생성계좌의 상위 레벨까지의 모든 노드를 검사한다.
-    # 1. 해당 검사 노드의 LEFT, RIGHT 중 소실적에 포함되는 리스트를 구한다.
-    # 2. 만일 LEFT = RIGHT 라면 RIGHT 가 소실적
-    # 3. 소실적의 갯수가 A 라면
-    # 4. A = 1  해당 노드의 후원 수당은 80
-    # 5. A>=2  then  A / 2 -> ((몫 X 2) x 90 ) + (나머지 X 80) 이 해당 노드의 후원 수당이 된다.
 
-    # 마지막으로 생성된 계좌의 레벨을 구한다.
-    last_node_object = _account_node_dic[_last_node_key]
-    level = last_node_object.level
-
-    # 소실적 계좌 리스트
-    small_side_list = []
-
-    # 소실적 계좌 리스트의 총 개수
-    small_side_list_count = 0
-
-    # 0레벨 부터 생성계좌의 상위 레벨까지의 각각의 레벨의 모든 노드를 검사한다.
-    #print("0 레벨부터 %d 레벨까지 각각의 노드를 검사" % (level - 1))
-    for index in range(0, level):
-        # 해당 레벨안에 있는 모든 노드를 돌면서 검사한다.
-        for base_node_index in _account_level_node_key_dic[index]:
-            #print("레벨 %d 안의  %d 노드 검사" % (index, base_node_index))
-            # 해당 노드의 LEFT, RIGHT 중 소실적에 포함되는 리스트와 갯수를 구한다.
-            (left_count, right_count) = calc_left_right_node_count(base_node_index, _account_node_dic)
-            if left_count < right_count:
-                small_side_list = _Left_Side_Node_Count_List
-                #print("레벨 %d 안의  소실적 노드 리스트" % index)
-                #print(small_side_list)
-            elif left_count > right_count:
-                small_side_list = _Right_Side_Node_Count_List
-                #print("레벨 %d 안의  소실적 노드 리스트" % index)
-                #print(small_side_list)
-            elif left_count == right_count:
-                small_side_list = _Right_Side_Node_Count_List
-                #print("레벨 %d 안의  소실적 노드 리스트" % index)
-                #print(small_side_list)
-
-            preorder_traverse()
-
-            small_side_list_count = len(small_side_list)
-
-            (quotient, remainder) = divmod(small_side_list_count, 2)
-
-            support_money = 0
-            if small_side_list_count == 1:
-                support_money = 80
-            elif small_side_list_count >= 2:
-                support_money = ((quotient * 2) * 90) + (remainder * 80)
-
-            # 해당 노드의 s_money 를 증가 시킨다.
-            #print("%d번 계좌에 셋팅될  후원 수당은 : %d" % (base_node_index, temp_s_money))
-            _account_node_dic[base_node_index].set_support_money(support_money)
-            small_side_list = []
-"""
 
 """"""
 # 계좌를 한꺼번에 셋팅하고 후원수당을 계산하는 함수. 두번째 버전
@@ -217,8 +157,11 @@ def support_money_setting(_last_node_key,
                                                                                                 _account_node_dic)
 
         # CASE 1
-        if (left_count == right_count) and (left_new_count == 1 or right_new_count == 1):
+        if (left_count == 1 and right_count == 1) and (left_new_count == 1 or right_new_count == 1):
             small_side_list = _Left_Side_Node_Support_Calc_False_List
+
+        # CASE 2
+        #if (left_count == right_count):
 
         preorder_traverse()
 
@@ -316,12 +259,14 @@ def preorder_traverse(base_node_index=-1, tree_object=None, flag=None):
         _Left_Side_Node_Count_List.append(tree_object.node_number)
         if tree_object.support_calc_used == False:
             _Left_Side_Node_Support_Calc_False_List.append(tree_object.node_number)
+            tree_object.support_calc_used = True
 
     elif flag is "right":
         # print("%d번 노드의 RIGHT 하위 노드: %d" % (base_node_index, tree_object.node_number))
         _Right_Side_Node_Count_List.append(tree_object.node_number)
         if tree_object.support_calc_used == False:
             _Right_Side_Node_Support_Calc_False_List.append(tree_object.node_number)
+            tree_object.support_calc_used = True
 
     preorder_traverse(base_node_index, tree_object.get_left_child_node(), flag)
     preorder_traverse(base_node_index, tree_object.get_right_child_node(), flag)
