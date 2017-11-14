8c884d11f1e0cbecb2cb20b427b397955ded70cd
luke <chargerunit@gmail.com>
2017-11-14 18:47:00 +0900

    .

--- Login_Test/Headless_Test.py
+++ Login_Test/Headless_Test.py
@@ -5,14 +5,24 @@ Mailbox = poplib.POP3('pop.naver.com')
 Mailbox.user("luke906")
 Mailbox.pass_('newlife8661!')
 numMessages = len(Mailbox.list()[1])
+body_contents = ""
 
 for i in range(numMessages):
     raw_email  = b"\n".join(Mailbox.retr(i+1)[1])
     parsed_email = email.message_from_bytes(raw_email)
+
     if parsed_email.is_multipart():
-        for payload in parsed_email.get_payload():
-            # if payload.is_multipart(): ...
-            print(payload.get_payload())
+        for part in parsed_email.walk():
+            ctype = part.get_content_type()
+            cdispo = str(part.get('Content-Disposition'))
+
+            # skip any text/plain (txt) attachments
+            if ctype == 'text/plain' and 'attachment' not in cdispo:
+                body_contents = part.get_payload(decode=True)  # decode
+                break
+    # not multipart - i.e. plain text, no attachments, keeping fingers crossed
     else:
-        print(parsed_email.get_payload())
-    #print(parsed_email)
+        body_contents = b.get_payload(decode=True)
+
+
+print(body_contents)
