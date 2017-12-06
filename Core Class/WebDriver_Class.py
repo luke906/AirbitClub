bc46939b2679e22cf687617913b540e28dad4b98
Luke LEE <chargerunit@gmail.com>
2017-12-06 22:03:10 +0900

    .

--- Core Class/WebDriver_Class.py
+++ Core Class/WebDriver_Class.py
@@ -2,13 +2,28 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from bs4 import BeautifulSoup
+#from browsermobproxy import Server
+from fake_useragent import UserAgent
+
 class WebDriver:
 
     def __init__(self, driver_path):
+        """
+        server = Server("../browsermob-proxy/bin/browsermob-proxy")
+        server.start()
+        proxy = server.create_proxy()
+        proxy.selenium_proxy()
+        capabilities = DesiredCapabilities.CHROME
+        proxy.add_to_capabilities(capabilities)
+        """
+
+        ua = UserAgent()
 
         self.DriverPath = driver_path
         self.chrome_options = Options()
+        #self.chrome_options.add_argument("--proxy-server={0}".format(proxy.proxy))
         self.chrome_options.add_argument("--start-maximized")
         self.chrome_options.add_argument("--disable-infobars")
         self.browser = webdriver.Chrome(executable_path=self.DriverPath, chrome_options=self.chrome_options)
--- Scrap Exam/Exam.py
+++ Scrap Exam/Exam.py
@@ -1,35 +1,8 @@
-from fpdf import FPDF
+from WebDriver_Class import WebDriver
 
-class PDF(FPDF):
-    def header(self):
-        # Logo
-        self.image('logo_pb.png', 10, 8, 33)
-        # Arial bold 15
-        self.add_font('Nanum', '', 'Nanum.ttf', uni=True)
-        self.set_font('Nanum', '', 20)
-        #self.set_font('Arial', 'B', 15)
-        # Move to the right
-        self.cell(80)
-        # Title
-        self.cell(30, 10, '전체 계좌 현황 보고서', 1, 0, 'C')
-        # Line break
-        self.ln(20)
 
-    # Page footer
-    def footer(self):
-        # Position at 1.5 cm from bottom
-        self.set_y(-15)
-        # Arial italic 8
-        self.set_font('Arial', 'I', 8)
-        # Page number
-        self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
+str_Chrome_Path = "../Selenium_Driver/chromedriver"
+str_AirBitClub_Login_URL = "https://www.bitbackoffice.com/auth/login"
 
-# Instantiation of inherited class
-pdf = PDF()
-pdf.alias_nb_pages()
-pdf.add_page()
-pdf.set_font('Times', '', 12)
-for i in range(1, 41):
-    pdf.cell(0, 10, 'Printing line number ' + str(i), 0, 1)
-pdf.output('tuto2.pdf', 'F')
-# pdf
+AirWebDriver = WebDriver(str_Chrome_Path)
+AirWebDriver.move_to_url(str_AirBitClub_Login_URL)
