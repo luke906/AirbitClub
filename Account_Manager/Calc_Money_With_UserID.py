f52a08dee739a6f2ce0a324dbfa8d926f104e8ad
lattepanda <chargerunit@gmail.com>
2017-12-13 23:16:21 +0900

    .

--- Account_Manager/Calc_Money_With_UserID.py
+++ Account_Manager/Calc_Money_With_UserID.py
@@ -430,7 +430,7 @@ if __name__ == "__main__":
     #scheduler.start_scheduler_cron(transfer_all_money_to_main_account, 'mon-sat', 2, 00)
     print("start scheduler transfer")
 
-    scheduler.start_scheduler_cron(get_total_bonus_money, 'mon-sat', 18, 48)
+    scheduler.start_scheduler_cron(get_total_bonus_money, 'mon-sat', 2, 0)
     print("start scheduler get_total_bonus_money")
 
 
