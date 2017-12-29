d9fd644e8733c10f9fe23d4ae50db0720c28bf39
luke <chargerunit@gmail.com>
2017-12-29 18:29:40 +0900

    .

--- Test Source/makecall.py
+++ Test Source/makecall.py
@@ -1,6 +1,8 @@
 # Download the Python helper library from twilio.com/docs/python/install
 from twilio.rest import Client
+from twilio.rest import TwilioRestClient
 
+"""
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "ACe9d8f7def064cdea868bba06b902ee1b"
 auth_token = "2088e2d9c761a09e0b00a9fa6d3f0314"
@@ -12,4 +14,19 @@ call = client.calls.create(
     url="http://demo.twilio.com/docs/voice.xml"
 )
 
-print(call.sid)
+print(call.sid
+"""
+
+
+#from_="+14242303478"
+
+# Set your account ID and authentication token.
+account_sid = "ACe9d8f7def064cdea868bba06b902ee1b"
+auth_token = "2088e2d9c761a09e0b00a9fa6d3f0314"
+
+client = Client(account_sid, auth_token)
+
+client.api.account.messages.create(
+    to="+821087821203",
+    from_="+14242303478",
+    body="hello")
