9d505fe15c50521508112388338125e5d1f0ef32
luke <chargerunit@gmail.com>
2017-12-29 11:35:01 +0900

    .

--- Core Class/WebDriver_Class.py
+++ Core Class/WebDriver_Class.py
@@ -28,7 +28,8 @@ class WebDriver_Manager:
         proxy.add_to_capabilities(capabilities)
         """
         user_name = os.getlogin()
-        user_path_name = "user-data-dir=C:/Users/" + user_name + "/AppData/Local/Google/Chrome/User Data"
+        #user_path_name = "user-data-dir=C:/Users/" + user_name + "/AppData/Local/Google/Chrome/User Data"
+        user_path_name = "../User_Profile"
 
         #ua = UserAgent()
         #capabilities = webdriver.DesiredCapabilities.CHROME
