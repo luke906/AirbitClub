de5f4c49b9c38117870835498db2606daebffdaf
luke <chargerunit@gmail.com>
2018-01-11 18:21:41 +0900

    .

--- Account_Manager/Calc_Money_With_UserID.py
+++ Account_Manager/Calc_Money_With_UserID.py
@@ -824,12 +824,13 @@ def report_account():
             str_repurchase_left_list += strtmp
 
     #  트랜스퍼 후 메인계좌 잔고 보고서 작성
-    str_main_transfer = "트랜스퍼 완료 후 메인계좌" + "(" + id_list[0] + ")" + " 잔고 현황\n"
+    str_report = ""
     str_transfer_date = "트랜스퍼 날짜 : " + nowDate + "\n"
-    str_total_account = "생성된 계좌의 총 갯수 : %d" % (len(id_list)) + "\n"
-
     str_today_rewards = "금일 트랜스퍼한 REWARDS 합계 : %.2f" % transfer_rewards_total.value + "$\n"
-    str_today_commisions = "금일 트랜스퍼한 COMMISIONS 합계 : %.2f" % transfer_commissions_total.value + "$\n"
+    str_today_commisions = "금일 트랜스퍼한 COMMISIONS 합계 : %.2f" % transfer_commissions_total.value + "$\n\n"
+
+    str_total_account = "현재 계좌의 총 갯수 : %d" % (len(id_list)) + "\n"
+    str_main_transfer = "트랜스퍼 완료 후 메인계좌" + "(" + id_list[0] + ")" + " 잔고 현황\n"
 
     str_rewards = "메인계좌 REWARDS : %.2f" % rewards.value + "$\n"
     str_commisions = "메인계좌 COMMISIONS : %.2f" % commissions.value + "$\n"
@@ -837,21 +838,22 @@ def report_account():
     str_savings = "메인계좌 SAVINGS : %.2f" % savings.value + "$\n"
     str_total = "총 인출 가능 달러(커미션 + 리워드) : %.2f" % (commissions.value + rewards.value) + "$\n"
 
-    str_main_transfer += str_today_rewards
-    str_main_transfer += str_today_commisions
+    str_report += str_transfer_date
+    str_report += str_today_rewards
+    str_report += str_today_commisions
 
-    str_main_transfer += str_total_account
-    str_main_transfer += str_total_account
+    str_report += str_total_account
+    str_report += str_main_transfer
 
-    str_main_transfer += str_rewards
-    str_main_transfer += str_commisions
-    str_main_transfer += str_cash
-    str_main_transfer += str_savings
-    str_main_transfer += str_total
+    str_report += str_rewards
+    str_report += str_commisions
+    str_report += str_cash
+    str_report += str_savings
+    str_report += str_total
 
     print(str_remaining_business_day_list)
     print(str_repurchase_left_list)
-    print(str_main_transfer)
+    print(str_report)
 
     # 집계를 마치고 변수를 초기화 한다.
     commissions.value = 0
@@ -878,7 +880,7 @@ def report_account():
     else:
         pdf.print_chapter_user('※ 75일 도래 전산비 납부 : 7일 전 계좌 리스트 ※', str_repurchase_left_list)
 
-    pdf.print_chapter_user('※ 트랜스퍼 완료 후 메인계좌 잔고 보고서 ※', str_main_transfer)
+    pdf.print_chapter_user('※ 트랜스퍼 완료 후 메인계좌 잔고 보고서 ※', str_report)
 
     rerport_filename = nowDate +  " " + user_name_list[0] +' 계좌현황 보고서.pdf'
 
